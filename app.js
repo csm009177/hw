e55e9b368d4a8ab34371bc08da8bd4ab51ac2053
csm <csm009177@gamil.com>
2023-10-05 20:40:54 +0900

    퇴근 푸시

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 const http = require('http');
 
-const serv = http.createServer(function(request, response){
-  let hw = `<!DOCTYPE html>
-  <html lang="kr">
+let serv = http.createServer(function(request, response){
+  let main = `<!DOCTYPE html>
+  <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>main</title>
-  <style>
-    *{background: black; color:aliceblue}
-    h1{color:gray}
-  </style>
   </head>
   <body>
     <div id="root">
-      <h1>Welcome to Main page</h1>
-      <div>
-        <a href="http://localhost:2023">next</a>
-      </div>
+      <h1>Welcome to main</h1>
+      <div><a href="http://localhost:1234/sofia"></a></div>
     </div>
   </body>
   </html>`
 
-  if(request==="/"){
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    response.end(hw);
-  }
-});
 
-serv.listen(2023);
+
+  if(response.url === "/"){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(main);
+  }
+
+})
+serv.listen(2020) 
