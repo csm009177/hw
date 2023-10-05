7c58c3b809d221bd8df3c7a01d28b541247eca48
csm <csm009177@gamil.com>
2023-10-05 17:53:13 +0900

    오타 수정

---
+++ dt9.js
@@ -0,0 +1,14 @@
+function surbur(callback) {
+  let request = {
+    insa : "hello"
+  };
+  let response = {
+    insa : "bye"
+  };
+  return callback(request, response);
+};
+
+surbur(function(request, response){
+  console.log(request.insa);
+  console.log(response.insa);
+});
