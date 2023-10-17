f61474332810881656df03cb34508be1ea3f5083
csm <csm009177@gmail.com>
2023-10-17 20:32:31 +0900

    make sub page and link

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const http = require('fs');
-let ContentType = require('./mod/ContentType.js')
-
+const fs = require('fs');
+let contenType = require('./mod/contenType.js');
+let PORT = 2020
 let serv = http.createServer(function(request, response){
   // doc make
   let main = `<!DOCTYPE html>
@@ -15,23 +15,41 @@ let serv = http.createServer(function(request, response){
   <body>
   <div id="root">
     <h1>Welcome to main</h1>
-    <div><a href="http://localhost:1234/sofia"></a></div>
+    <div><a href="http://localhost:${PORT}/sofia"></a></div>
     </div>
     </body>
     </html>`
+
+  let sofia = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>main</title>
+  </head>
+  <body>
+  <div id="root">
+    <h1>Welcome to main</h1>
+    <div><a href="http://localhost:${PORT}/">main</a></div>
+    </div>
+    </body>
+    </html>`
+  // main page
   // check mothod 
   if(request.method==='GET' && request.url=== '/'){
     // err find
-    if (err) {
-      console.log('err exist');
-    } else { 
-    response.writeHead(200, ContentType)
-  response.end(main);
+    response.writeHead(200, contenType)
+    response.end(main);
+    } else if(request.method==='GET' && request.url=== '/sofia'){
+      // err find
+      response.writeHead(200, contenType)
+      response.end(sofia);
+      };
+});
 
-  }}})
 
 
-let PORT = 2020
 serv.listen(PORT, () => {
   console.log(`
 now server is working
